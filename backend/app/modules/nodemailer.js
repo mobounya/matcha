c9d0a0ee6ab1d560e518d4444b21a1070fd8e1b6
afaddoul <faddoulachraf39@gmail.com>
2021-12-22 22:16:05 +0100

    Fix Typo: nodemailer module, sendMail fuction firt param

--- backend/app/modules/nodemailer.js
+++ backend/app/modules/nodemailer.js
@@ -1,26 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
-  }
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+    }
 });
 
-function sendMail(recepient, subject, text) {
-  let mailOptions = {
-    from: process.env.MAIL_USERNAME,
-    to: recepient,
-    subject: subject,
-    html: `<p>${text}</p>`
-  };
+function sendMail(recipient, subject, text) {
+    let mailOptions = {
+        from: process.env.MAIL_USERNAME,
+        to: recipient,
+        subject: subject,
+        html: `<p>${text}</p>`
+    };
 
-  return transporter.sendMail(mailOptions);
+    return transporter.sendMail(mailOptions);
 }
 
 module.exports = { sendMail };
