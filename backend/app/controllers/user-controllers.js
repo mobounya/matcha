263817b2987b995a5501f47f47bb71d69cb5e25e
afaddoul <faddoulachraf39@gmail.com>
2021-12-29 02:02:27 +0100

    add send authentication token controller and format coontrollers with prettier

--- backend/app/controllers/user-controllers.js
+++ backend/app/controllers/user-controllers.js
@@ -1,51 +1,69 @@
-const db = require("../database/db");
-const httpStatus = require("../lib/http-status");
+  const db = require("../database/db");
+  const httpStatus = require("../lib/http-status");
 
-async function insertUser(request, response) {
-  try {
-    const res = await db.createUser(request.body);
-    response.status(httpStatus.HTTP_CREATED).json({
-      message: "User created successfully",
-      data: res.rows[0]
-    });
-  } catch (e) {
-    response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
-      error: "something went wrong"
-    });
+  async function insertUser(request, response) {
+      try {
+          const res = await db.createUser(request.body);
+          response.status(httpStatus.HTTP_CREATED).json({
+              message: "User created successfully",
+              data: res.rows[0]
+          });
+      } catch (e) {
+          response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
+              error: "something went wrong"
+          });
+      }
   }
-}
 
-async function verifyEmail(request, response) {
-  try {
-    const email = request.decodedPayload.email;
-    const isVerified = true;
-    await db.changeEmailVerifiedValue(email, isVerified);
-    response.status(httpStatus.HTTP_OK).json({
-      message: "email is verified"
-    });
-  } catch (error) {
-    response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
-      error: "something went wrong"
-    });
+  async function verifyEmail(request, response) {
+      try {
+          const email = request.decodedPayload.email;
+          const isVerified = true;
+          await db.changeEmailVerifiedValue(email, isVerified);
+          response.status(httpStatus.HTTP_OK).json({
+              message: "email is verified"
+          });
+      } catch (error) {
+          response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
+              error: "something went wrong"
+          });
+      }
   }
-}
 
-function changeUserPassword(getUserEmail) {
-  return async (request, response) => {
-    try {
-      const email = getUserEmail(request);
-      const password = request.body.password;
+  function changeUserPassword(getUserEmail) {
+      return async(request, response) => {
+          try {
+              const email = getUserEmail(request);
+              const password = request.body.password;
 
-      await db.changeUserPassword(email, password);
-      response.status(httpStatus.HTTP_OK).json({
-        message: "password updated successfully"
-      });
-    } catch (error) {
-      response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
-        error: "something went wrong"
-      });
-    }
-  };
-}
+              await db.changeUserPassword(email, password);
+              response.status(httpStatus.HTTP_OK).json({
+                  message: "password updated successfully"
+              });
+          } catch (error) {
+              response.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
+                  error: "something went wrong"
+              });
+          }
+      };
+  }
 
-module.exports = { insertUser, verifyEmail, changeUserPassword };
+  async function sendAuthToken(req, res) {
+      try {
+          const token = req.token
+          const oneHourInSeconds = 60 * 60;
+          res.cookie('token', token, {
+              httpOnly: true,
+              sameSite: 'strict',
+              maxAge: oneHourInSeconds,
+              secure: process.env.NODE_ENV === 'production' ? true : false
+          });
+          res.json({ message: "user authenticated successfully" });
+      } catch (e) {
+          return res.status(httpStatus.HTTP_INTERNAL_SERVER_ERROR).json({
+              error: "something went wrong"
+          });
+      }
+  }
+
+  module.exports = { insertUser, verifyEmail, changeUserPassword, sendAuthToken };
