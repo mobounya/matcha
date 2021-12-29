634c1f06fb3d6da09155e2a77eab5098b833cc22
afaddoul <faddoulachraf39@gmail.com>
2021-12-29 02:00:18 +0100

    code linting with prettier

--- backend/app/middlewares/token-validator-middleware.js
+++ backend/app/middlewares/token-validator-middleware.js
@@ -2,19 +2,19 @@ const { jwtVerifyToken } = require("../modules/jwt");
 const httpStatus = require("../lib/http-status");
 
 function verifyToken(jwtSecretKey, field) {
-  return async (request, response, next) => {
-    try {
-      const token = request[field].token;
+    return async(request, response, next) => {
+        try {
+            const token = request[field].token;
 
-      const decodedPayload = await jwtVerifyToken(token, jwtSecretKey);
-      request.decodedPayload = decodedPayload;
-      next();
-    } catch (e) {
-      response.status(httpStatus.HTTP_BAD_REQUEST).json({
-        error: "Invalid token"
-      });
-    }
-  };
+            const decodedPayload = await jwtVerifyToken(token, jwtSecretKey);
+            request.decodedPayload = decodedPayload;
+            next();
+        } catch (e) {
+            response.status(httpStatus.HTTP_BAD_REQUEST).json({
+                error: "Invalid token"
+            });
+        }
+    };
 }
 
 module.exports = { verifyToken };
