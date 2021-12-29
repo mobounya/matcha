7a92e9b54399e45d2a9aabd70a61be4bac16d44d
afaddoul <faddoulachraf39@gmail.com>
2021-12-29 22:43:17 +0100

    change jwt secret key envirement variable

--- backend/app/middlewares/auth-middleware.js
+++ backend/app/middlewares/auth-middleware.js
@@ -1,7 +1,7 @@
 const { jwtSignPayload } = require("./../modules/jwt")
 
 function generateToken(req, res, next) {
-    const secretKey = process.env.JWT_EMAIL_VERIFICATION_SECRET_KEY;
+    const secretKey = process.env.JWT_APP_SECRET_KEY;
     const payload = { userId: req.userId };
     const expiration = "1h";
     const token = jwtSignPayload(payload, secretKey, expiration);
