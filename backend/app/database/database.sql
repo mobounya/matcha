abe081598dbbe364f0e667b87fbf8805f1c9c529
afaddoul <faddoulachraf39@gmail.com>
2022-01-22 19:53:37 +0100

    move first and last name from users to profiles table and add clean db queries

--- backend/app/database/database.sql
+++ backend/app/database/database.sql
@@ -3,10 +3,8 @@ CREATE TYPE genders AS ENUM ('f', 'm');
 
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL NOT NULL PRIMARY KEY,
-  first_name varchar(35) NOT NULL,
-  last_name varchar(35) NOT NULL,
-  username varchar(30) NOT NULL,
   email varchar(254) NOT NULL,
+  username varchar(30) NOT NULL,
   password varchar(64) NOT NULL,
   email_verified boolean DEFAULT 'f',
   is_active boolean DEFAULT 'f'
@@ -15,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS tags (
   id SERIAL NOT NULL PRIMARY KEY,
   tag varchar(10) UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS user_tags (
   user_id INT NOT NULL,
@@ -23,13 +21,24 @@ CREATE TABLE IF NOT EXISTS user_tags (
   FOREIGN KEY(user_id) REFERENCES users(id),
   FOREIGN KEY(tag_id) REFERENCES tags(id),
   UNIQUE (user_id, tag_id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS profiles (
   id INT NOT NULL PRIMARY KEY,
+  first_name varchar(35) NOT NULL,
+  last_name varchar(35) NOT NULL,
   gender genders NULL,
   sexual_preference sexual_preferences NULL,
   biography TEXT NULL,
   profile_compeleted boolean DEFAULT 'f',
   FOREIGN KEY(id) REFERENCES users(id)
 );
+
+-- Drop all tables --
+DROP TABLE pictures;
+DROP TABLE profiles;
+DROP TYPE genders;
+DROP TYPE sexual_preferences;
+DROP TABLE user_tags;
+DROP TABLE tags;
+DROP TABLE users;
